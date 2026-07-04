7906595935fb888a157779b023458d1e877a68f5
dmahacker <njwuqi123@126.com>
2026-07-04 13:42:21 +0800

    Dispatch native shortcut actions

--- wiliwili/source/utils/base_shortcut_helper.cpp
+++ wiliwili/source/utils/base_shortcut_helper.cpp
@@ -34,11 +34,28 @@ static bool dispatchButtonOnce(const brls::BrlsKeyCombination& shortcut, const b
     return true;
 }
 
+static bool isNativeShortcutBinding(const ShortcutBinding& binding) {
+    if (binding.device == ShortcutDevice::WindowsAppCommand || binding.device == ShortcutDevice::WindowsRawHid) return true;
+    return binding.device == ShortcutDevice::Keyboard && binding.key.code == brls::BRLS_KBD_KEY_UNKNOWN &&
+           binding.nativeCode > 0;
+}
+
+static std::string nativeSafeConfig(const std::string& config, const std::string& defaultConfig) {
+    const ShortcutBinding binding = ShortcutBindingHelper::parseBinding(config);
+    return isNativeShortcutBinding(binding) ? defaultConfig : config;
+}
+
 static void loadBaseShortcut(ShortcutAction action, const std::string& config) {
     ShortcutCaptureHelper::setNativeShortcut(action, ShortcutBindingHelper::parseBinding(config));
 }
 
 static bool dispatchNativeBaseShortcut(ShortcutAction action) {
+    auto dispatchKeyboardShortcut = [](const brls::BrlsKeyCombination& shortcut) {
+        if (shortcut.code == brls::BRLS_KBD_KEY_UNKNOWN) return false;
+        brls::Application::onKeyboardPressed(shortcut, false);
+        return true;
+    };
+
     switch (action) {
         case ShortcutAction::Confirm:
             brls::Application::onControllerButtonPressed(brls::BUTTON_A, false);
@@ -58,6 +75,44 @@ static bool dispatchNativeBaseShortcut(ShortcutAction action) {
         case ShortcutAction::NavigateRight:
             brls::Application::onControllerButtonPressed(brls::BUTTON_NAV_RIGHT, false);
             return true;
+        case ShortcutAction::Refresh:
+            return dispatchKeyboardShortcut(ShortcutHelper::getRefresh());
+        case ShortcutAction::Search:
+            return dispatchKeyboardShortcut(ShortcutHelper::getSearch());
+        case ShortcutAction::Last:
+            return dispatchKeyboardShortcut(ShortcutHelper::getLast());
+        case ShortcutAction::Next:
+            return dispatchKeyboardShortcut(ShortcutHelper::getNext());
+        case ShortcutAction::LastSub:
+            return dispatchKeyboardShortcut(ShortcutHelper::getLastSub());
+        case ShortcutAction::NextSub:
+            return dispatchKeyboardShortcut(ShortcutHelper::getNextSub());
+        case ShortcutAction::VolumeUp:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVolumeUp());
+        case ShortcutAction::VolumeDown:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVolumeDown());
+        case ShortcutAction::VideoProfile:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoProfile());
+        case ShortcutAction::Danmaku:
+            return dispatchKeyboardShortcut(ShortcutHelper::getDanmaku());
+        case ShortcutAction::Playlist:
+            return dispatchKeyboardShortcut(ShortcutHelper::getPlaylist());
+        case ShortcutAction::Forward:
+            return dispatchKeyboardShortcut(ShortcutHelper::getForward());
+        case ShortcutAction::Rewind:
+            return dispatchKeyboardShortcut(ShortcutHelper::getRewind());
+        case ShortcutAction::Setting:
+            return dispatchKeyboardShortcut(ShortcutHelper::getSetting());
+        case ShortcutAction::VideoQuality:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoQuality());
+        case ShortcutAction::VideoSpeed:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoSpeed());
+        case ShortcutAction::VideoSpeedUp:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoSpeedUp());
+        case ShortcutAction::VideoOsd:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoOsd());
+        case ShortcutAction::VideoPause:
+            return dispatchKeyboardShortcut(ShortcutHelper::getVideoPause());
         default:
             return false;
     }
@@ -66,19 +121,26 @@ static bool dispatchNativeBaseShortcut(ShortcutAction action) {
 void BaseShortcutHelper::loadFromConfig(ProgramConfig& config) {
     ShortcutCaptureHelper::setNativeDispatchCallback(dispatchNativeBaseShortcut);
 
-    const std::string confirm = config.getSettingItem(SettingItem::SHORTCUT_CONFIRM, std::string{"enter"});
-    const std::string back = config.getSettingItem(SettingItem::SHORTCUT_BACK, std::string{"escape"});
-    const std::string navigateUp = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_UP, std::string{"up"});
-    const std::string navigateDown = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_DOWN, std::string{"down"});
-    const std::string navigateLeft = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_LEFT, std::string{"left"});
-    const std::string navigateRight = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_RIGHT, std::string{"right"});
+    const std::string confirmDefault = "enter";
+    const std::string backDefault = "escape";
+    const std::string navigateUpDefault = "up";
+    const std::string navigateDownDefault = "down";
+    const std::string navigateLeftDefault = "left";
+    const std::string navigateRightDefault = "right";
 
-    setConfirm(confirm);
-    setBack(back);
-    setNavigateUp(navigateUp);
-    setNavigateDown(navigateDown);
-    setNavigateLeft(navigateLeft);
-    setNavigateRight(navigateRight);
+    const std::string confirm = config.getSettingItem(SettingItem::SHORTCUT_CONFIRM, confirmDefault);
+    const std::string back = config.getSettingItem(SettingItem::SHORTCUT_BACK, backDefault);
+    const std::string navigateUp = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_UP, navigateUpDefault);
+    const std::string navigateDown = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_DOWN, navigateDownDefault);
+    const std::string navigateLeft = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_LEFT, navigateLeftDefault);
+    const std::string navigateRight = config.getSettingItem(SettingItem::SHORTCUT_NAVIGATE_RIGHT, navigateRightDefault);
+
+    setConfirm(nativeSafeConfig(confirm, confirmDefault));
+    setBack(nativeSafeConfig(back, backDefault));
+    setNavigateUp(nativeSafeConfig(navigateUp, navigateUpDefault));
+    setNavigateDown(nativeSafeConfig(navigateDown, navigateDownDefault));
+    setNavigateLeft(nativeSafeConfig(navigateLeft, navigateLeftDefault));
+    setNavigateRight(nativeSafeConfig(navigateRight, navigateRightDefault));
 
     loadBaseShortcut(ShortcutAction::Confirm, confirm);
     loadBaseShortcut(ShortcutAction::Back, back);
